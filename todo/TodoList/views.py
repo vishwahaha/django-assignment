eaa8e9eba899ad438691af3d70d8d75d5d01d7d3
Vishwa Prakash <vishwaprakash2003@gmail.com>
2021-08-17 11:56:04 +0530

    second it

--- todo/TodoList/views.py
+++ todo/TodoList/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import TodoList, ListItem
@@ -6,6 +6,9 @@ from .models import TodoList, ListItem
 # Create your views here.
 
 def index(request):
+    """
+    Shows list of all todo lists
+    """
     todolists = TodoList.objects.all()
 
     context = {
@@ -14,6 +17,10 @@ def index(request):
     return render(request, 'TodoList/index.html', context)
 
 def detail(request, list_id):
+    """
+    Detailed view of a list, a form to add more items to list.
+    Also, with each list option to edit the list item and delete it is present this is handled by the post request
+    """
     if request.method == 'POST':
         newItem = request.POST['addItem']
         dueDate = request.POST['duedate']
@@ -39,6 +46,9 @@ def detail(request, list_id):
         return render(request, 'TodoList/detail.html', context)
 
 def create(request):
+    """
+    Handles simple form to create a new list
+    """
     if request.method == 'POST':
         name = request.POST['name']
         newList = TodoList.objects.create(list_name = name)
@@ -51,14 +61,22 @@ def create(request):
         return render(request, 'TodoList/create.html')
 
 def deleteList(request, list_id):
+    """
+    Deletes a list with a form by taking the list_id
+    """
     if request.method == "POST":
         list = get_object_or_404(TodoList, id = list_id)
         list.delete()
         return HttpResponseRedirect(reverse('TodoList:index'))
-    return HttpResponseRedirect(reverse('TodoList:index'))
+    else:
+        list = get_object_or_404(TodoList, id = list_id)
+        return HttpResponseRedirect(reverse('TodoList:index'))
     
 
 def updateItem(request, list_id, item_id):
+    """
+    Handles form which updates todo item
+    """
     if request.method == "POST":
         todoList = get_object_or_404(TodoList, id = list_id)
         item = ListItem.objects.filter(parent_list = todoList, id = item_id)
@@ -74,7 +92,7 @@ def updateItem(request, list_id, item_id):
 
     else:
         todoList = get_object_or_404(TodoList, id = list_id)
-        item = ListItem.objects.filter(parent_list = todoList, id = item_id)[0]
+        item = get_object_or_404(ListItem, id = item_id)
 
         context = {
             'todoList' : todoList,
@@ -83,10 +101,16 @@ def updateItem(request, list_id, item_id):
         return render(request, 'TodoList/update.html', context)
 
 def deleteItem(request, list_id, item_id):
+    """
+    Deletes list item by finding it from list_id
+    """
     if request.method == "POST":
         todoList = get_object_or_404(TodoList, id = list_id)
-        to_del = ListItem.objects.filter(parent_list = todoList, id = item_id)
+        to_del = get_object_or_404(ListItem, id = item_id)
         to_del.delete()
         return HttpResponseRedirect(reverse('TodoList:detail', args=(list_id,)))
         
-    return HttpResponseRedirect(reverse('TodoList:detail', args=(list_id,)))
+    else:
+        todoList = get_object_or_404(TodoList, id = list_id)
+        to_del = get_object_or_404(ListItem, id = item_id)
+        return HttpResponse(reverse('TodoList:detail', args=(list_id,)))
